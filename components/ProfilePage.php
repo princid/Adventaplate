bf04b0565a18b7a28ba2cac33fce60db91b8857f
princid <princid85@gmail.com>
2023-10-20 18:17:23 +0530

    Improved the UI of Profile Page

--- components/ProfilePage.php
+++ components/ProfilePage.php
@@ -36,180 +36,177 @@ if (empty($_SESSION['id'])) {
 
 
 <div class="profile__container">
-    <!-- <form action="<?php #$_SERVER['PHP_SELF']; ?>" method="post" id="form" enctype="multipart/form-data"> -->
+    <!-- <form action="<?php #$_SERVER['PHP_SELF']; 
+                        ?>" method="post" id="form" enctype="multipart/form-data"> -->
 
-        <div class="userProfile">
-            <!-- <h2>My Information</h2> -->
-            
+    <div class="userProfile">
+        <!-- <h2>My Information</h2> -->
 
-            
-            <div class="userImage">
-                <!-- Profile Image -->
-                <?php if (!empty($new_image))  { ?>
-                    <img src="<?php echo BASE_URL ?>assets/uploading/<?php echo $id. "/". $new_image; ?>" alt="">
-                    
-                <?php } else { ?>
-                    <img src="<?php echo BASE_URL ?>assets/uploading/userDummy.png">
-                <?php } ?>
-            </div>
 
-            <?php
-                echo strtoupper("<h1>$username</h1>");
-                echo $bio;
-            ?>
 
-            <!-- <hr> -->
-            
-            <!-- <div class="preview"></div> -->
+        <div class="userImage">
+            <!-- Profile Image -->
+            <?php if (!empty($new_image)) { ?>
+                <img src="<?php echo BASE_URL ?>assets/uploading/<?php echo $id . "/" . $new_image; ?>" alt="">
 
-            <!-- Displaying User's info which is fetched from the database -->
-            <div class="userInfo">
+            <?php } else { ?>
+                <img src="<?php echo BASE_URL ?>assets/uploading/userDummy.png">
+            <?php } ?>
+        </div>
 
-                <!-- <h4>MY DETAILS</h4> -->
+        <?php
+        echo strtoupper("<h1>$username</h1>");
+        echo $bio;
+        ?>
 
-                <table class="userTable">
-                    <tbody>
-                        
-                        <!-- Email -->
-                        <tr class="tab_row">
-                            <th>Email </th>
-                            <td>
-                                <?php echo "$email"; ?>
-                            </td>
-                        </tr>
-                        
-                        <!-- Phone -->
-                        <tr class="tab_row">
-                            <th>Phone </th>
-                            <td>
-                                <?php echo "$phone"; ?>
-                            </td>
-                        </tr>
-                        
-                        <!-- Age -->
-                        <tr class="tab_row">
-                            <th>Age </th>
-                            <td>
-                                <?php echo "$age"; ?>
-                            </td>
-                        </tr>
-                        
-                        <!-- D.O.B. -->
-                        <tr class="tab_row">
-                            <th>D.O.B. </th>
-                            <td>
-                                <?php
-                                    if(empty($dob)){
-                                        echo "-";
-                                    } 
-                                    else{
-                                        echo "$dob"; 
-                                    }
-                                ?>
-                            </td>
-                        </tr>
-                        
-                        <!-- Gender -->
-                        <tr class="tab_row">
-                            <th>Gender </th>
-                            <td>
-                                <?php 
-                                    if(empty($gender)){
-                                        echo "-";
-                                    }
-                                    else{
-                                        echo "$gender"; 
-                                    }
-                                ?>
-                            </td>
-                        </tr>
-                        
-                        <!-- Subjects -->
-                        <tr class="tab_row">
-                            <th>Fav. Subjects </th>
-                            <td>
-                                <?php 
-                                    if(empty($selectedSubjects)){
-                                        echo "-";
-                                    }
-                                    else{
-                                        $subStr = implode(", ", $selectedSubjects);
-                                        echo "$subStr"; 
-                                    }
-                                ?>
-                            </td>
-                        </tr>
-                        
-                        <!-- Hobbies -->
-                        <tr class="tab_row">
-                            <th>Hobbies </th>
-                            <td>
-                                <?php 
+        <!-- <hr> -->
 
-                                    if(empty($user_hobbies)){
-                                        echo "-";
-                                    }
-                                    else{
-                                        $hobbyStr = implode(", ", $user_hobbies);
-                                        echo "$hobbyStr";
-                                    }
-                                ?>
-                            </td>
-                        </tr>
-                        
-                        <!-- Street Address with PIN code -->
-                        <tr class="tab_row">
-                            <th>Address </th>
-                            <td>
-                                <?php 
+        <!-- <div class="preview"></div> -->
 
-                                    if(empty($street) && empty($city) && empty($state) && empty($country) && empty($pin)){
-                                        echo "-";
-                                    }
-                                    else{
-                                        echo "$street, $city, $state, $country, $pin";
-                                    }
-                                ?>
-                            </td>
-                        </tr>
-                        
-                        
-                    </tbody>
-                </table>
-    
-            </div>
+        <!-- Displaying User's info which is fetched from the database -->
+        <div class="userInfo">
 
-            <a href="<?php echo BASE_URL ?>components/ProfileForm.php"><button class="editBtn">EDIT PROFILE </button></a>
-            <!-- <a href="<?php #echo BASE_URL ?>components/SignOut.php"><button class="logOut_btn">LOG OUT</button></a> -->
+            <!-- <h4>MY DETAILS</h4> -->
 
-            <!-- Button trigger modal -->
-            <button type="button" class="logOut_btn" data-bs-toggle="modal" data-bs-target="#delModal">
-                  LOG OUT
-                </button>
+            <table class="userTable">
+                <tbody>
 
-                <!-- Modal -->
-                <div class="modal fade" id="delModal" tabindex="-1" aria-labelledby="" aria-hidden="true">
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-                      <div class="modal-header">
+                    <!-- Email -->
+                    <tr class="tab_row">
+                        <th>Email </th>
+                        <td>
+                            <?php echo "$email"; ?>
+                        </td>
+                    </tr>
+
+                    <!-- Phone -->
+                    <tr class="tab_row">
+                        <th>Phone </th>
+                        <td>
+                            <?php echo "$phone"; ?>
+                        </td>
+                    </tr>
+
+                    <!-- Age -->
+                    <tr class="tab_row">
+                        <th>Age </th>
+                        <td>
+                            <?php echo "$age"; ?>
+                        </td>
+                    </tr>
+
+                    <!-- D.O.B. -->
+                    <tr class="tab_row">
+                        <th>D.O.B. </th>
+                        <td>
+                            <?php
+                            if (empty($dob)) {
+                                echo "-";
+                            } else {
+                                echo "$dob";
+                            }
+                            ?>
+                        </td>
+                    </tr>
+
+                    <!-- Gender -->
+                    <tr class="tab_row">
+                        <th>Gender </th>
+                        <td>
+                            <?php
+                            if (empty($gender)) {
+                                echo "-";
+                            } else {
+                                echo "$gender";
+                            }
+                            ?>
+                        </td>
+                    </tr>
+
+                    <!-- Subjects -->
+                    <tr class="tab_row">
+                        <th>Fav. Subjects </th>
+                        <td>
+                            <?php
+                            if (empty($selectedSubjects)) {
+                                echo "-";
+                            } else {
+                                $subStr = implode(", ", $selectedSubjects);
+                                echo "$subStr";
+                            }
+                            ?>
+                        </td>
+                    </tr>
+
+                    <!-- Hobbies -->
+                    <tr class="tab_row">
+                        <th>Hobbies </th>
+                        <td>
+                            <?php
+
+                            if (empty($user_hobbies)) {
+                                echo "-";
+                            } else {
+                                $hobbyStr = implode(", ", $user_hobbies);
+                                echo "$hobbyStr";
+                            }
+                            ?>
+                        </td>
+                    </tr>
+
+                    <!-- Street Address with PIN code -->
+                    <tr class="tab_row">
+                        <th>Address </th>
+                        <td>
+                            <?php
+
+                            if (empty($street) && empty($city) && empty($state) && empty($country) && empty($pin)) {
+                                echo "-";
+                            } else {
+                                echo "$street, $city, $state, $country, $pin";
+                            }
+                            ?>
+                        </td>
+                    </tr>
+
+
+                </tbody>
+            </table>
+
+        </div>
+
+        <a href="<?php echo BASE_URL ?>components/ProfileForm.php"><button class="editBtn">EDIT PROFILE </button></a>
+        <!-- <a href="<?php #echo BASE_URL 
+                        ?>components/SignOut.php"><button class="logOut_btn">LOG OUT</button></a> -->
+
+        <!-- Button trigger modal -->
+        <button type="button" class="logOut_btn" data-bs-toggle="modal" data-bs-target="#delModal">
+            LOG OUT
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="delModal" tabindex="-1" aria-labelledby="" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
                         <h2 class="modal-title fs-5 text-dark" id="">Are you sure?</h2>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                      </div>
-                      <div class="modal-body text-dark">
+                    </div>
+                    <div class="modal-body text-dark">
                         <p>Do you really want to log out of your account?</p>
-                      </div>
-                      <div class="modal-footer">
+                    </div>
+                    <div class="modal-footer">
                         <button type="button" class="closeBtn" data-bs-dismiss="modal">CLOSE</button>
                         <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
                         <a class="delBtn" href="<?php echo BASE_URL ?>components/SignOut.php">LOG OUT</a>
-                      </div>
                     </div>
-                  </div>
                 </div>
+            </div>
         </div>
+    </div>
 
 
-        <!--  -->
+    <!--  -->
 </div>
 
 <!-- </div> -->
@@ -234,4 +231,3 @@ if (empty($_SESSION['id'])) {
     //     imageDiv.appendChild(newImg);
     // }
 </script>
-
