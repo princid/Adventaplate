d68f9aed701a9bc1dbcca7d88a42e412bae69827
princid <princid85@gmail.com>
2023-10-20 18:18:55 +0530

    Implemented role update handler

--- src/controller/userDataForAdmin.php
+++ src/controller/userDataForAdmin.php
@@ -1,11 +1,25 @@
 <?php
 
-if(!empty($_SESSION)){
-    $id = $_SESSION['id'];
+session_start();
 
-    $fetch_name = "SELECT * FROM users_table";
-    $fetch_name_run = mysqli_query($conn, $fetch_name);
+require ('../../config/connectDB.php');
+
+if (isset($_POST["update_user_role"])) {
+
+    $user_data_id = $_POST['user_data_id'];
+    $new_role = $_POST['role'];
+
+    $update_role = "UPDATE users_table SET user_role_status = '$new_role' WHERE id = '$user_data_id' ";
+   
+    $update_role_run = mysqli_query($conn, $update_role);
+    
+    if ($update_role_run) {
+        header("location: http://localhost/PHP_Assesments/Adventaplate/admin/Dashboard.php");
+        exit;
+    }
 
 }
+
+
 
 ?>
